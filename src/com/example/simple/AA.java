821c17b1293bd7d021dfc6a9a2b1fd55fac832eb
Stanley <stanleychen@Objectivasoftware.com>
2016-04-19 16:43:05 +0800

    test

--- src/com/example/simple/AA.java
+++ src/com/example/simple/AA.java
@@ -2,12 +2,15 @@ package com.example.simple;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.StrBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class AA
 {
+	private static final Logger logger = LoggerFactory.getLogger(AA.class);
 	private String name;
-	
+
 	public String appendNames()
 	{
 		StrBuilder sb = new StrBuilder();
@@ -15,6 +18,10 @@ public class AA
 		if (StringUtils.isNotEmpty(getName()))
 		{
 			sb.append(" " + getName());
+		}
+		if (logger.isDebugEnabled())
+		{
+			logger.debug(sb.toString());
 		}
 		return sb.toString();
 	}
